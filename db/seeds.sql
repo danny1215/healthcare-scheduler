bf1df2793df07ba0a5355b825bd1014d7f032f30
Daniel Mestawat <dannymamusha@gmail.com>
2021-03-08 23:39:28 -0500

    update

--- db/seeds.sql
+++ db/seeds.sql
@@ -8,10 +8,3 @@ INSERT INTO providers (provider_First_name, provider_last_name,email,license,mon
 INSERT INTO providers (provider_First_name, provider_last_name,email,license,monday,tuesday,wednesday,thursday,friday,saturday,sunday, createdAt, updatedAt) values ('Derek', 'Barnett','derekbarnett@gmail.com','CRN',false,true,false,true,false,true,false,"2021-03-02 17:47:12", "2021-03-02 17:47:12");
 INSERT INTO providers (provider_First_name, provider_last_name,email,license,monday,tuesday,wednesday,thursday,friday,saturday,sunday, createdAt, updatedAt) values ('Shaun', 'Bradley','shaunbradley@gmail.com','CRN',false,false,false,false,true,true,true,"2021-03-02 17:47:12", "2021-03-02 17:47:12");
 INSERT INTO providers (provider_First_name, provider_last_name,email,license,monday,tuesday,wednesday,thursday,friday,saturday,sunday, createdAt, updatedAt) values ('Corey', 'Clement','coreyclement@gmail.com','CRN',true,true,true,true,false,false,false,"2021-03-02 17:47:12", "2021-03-02 17:47:12");
-
--- INSERT INTO Patient (title, authorId) values ('Pride and Prejudice', 1);
--- INSERT INTO Patient (title, authorId) values ('Emma', 1);
--- INSERT INTO Patient (title, authorId) values ('The Adventures of Tom Sawyer', 2);
--- INSERT INTO Patient (title, authorId) values ('Adventures of Huckleberry Finn', 2);
--- INSERT INTO Patient (title, authorId) values ('Alice''s Adventures in Wonderland', 3);
--- INSERT INTO Patient (title, authorId) values ('Dracula', null);
